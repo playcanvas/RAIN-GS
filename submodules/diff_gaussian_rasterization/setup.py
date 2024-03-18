b7e6dd61024d95b1b79b0a2fc0802b902721d38b
Donovan Hutchence <slimbuck7@gmail.com>
2024-03-18 16:25:22 +0000

    paths

--- submodules/diff_gaussian_rasterization/setup.py
+++ submodules/diff_gaussian_rasterization/setup.py
@@ -10,10 +10,10 @@ build_dir = _get_build_directory(name, verbose=False)
 _C = load(
     name=name,
     sources=[
-            "submodules/diff_gaussian_rasterization/cuda_rasterizer/rasterizer_impl.cu",
-            "submodules/diff_gaussian_rasterization/cuda_rasterizer/forward.cu",
-            "submodules/diff_gaussian_rasterization/cuda_rasterizer/backward.cu",
-            "submodules/diff_gaussian_rasterization/rasterize_points.cu",
-            "submodules/diff_gaussian_rasterization/ext.cpp"],
+            "cuda_rasterizer/rasterizer_impl.cu",
+            "cuda_rasterizer/forward.cu",
+            "cuda_rasterizer/backward.cu",
+            "rasterize_points.cu",
+            "ext.cpp"],
     extra_cuda_cflags=["-I" + os.path.join(os.path.dirname(os.path.abspath(__file__)), "third_party/glm/")],
 )
